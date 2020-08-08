acc2be3a51b53bf17a8cf37e07b8e546cfdc1f62
ishancoderr <ishan.weerakoon11@gmail.com>
2020-08-08 18:56:23 +0700

    Test

--- app/src/main/java/com/example/it17026758/MainActivity.java
+++ app/src/main/java/com/example/it17026758/MainActivity.java
@@ -3,12 +3,55 @@ package com.example.it17026758;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("Lifecycle", "Oncreate() invoked");
+        textView2 = findViewById(R.id.textview2);
+        textView2.setText(R.string.Msg2);
+    }
+
+    public void onRestart()
+    {
+        super.onRestart();
+        Log.i("Lifecycle", "onRestart() invoked" );
+        }
+
+    public void onStart()
+    {
+
+        super.onStart();
+        Log.i("Lifecycle", "Oncreate() invoked" );
+    }
+    public void onResume()
+    {
+
+        super.onResume();
+        Log.i("Lifecycle", "onResume() invoked" );
+    }
+    public void onPause()
+    {
+
+        super.onPause();
+        Log.i("Lifecycle", "onPause() invoked" );
+    }
+    public void onStop()
+    {
+
+        super.onStop();
+        Log.i("Lifecycle", "onStop() invoked" );
+    }
+    public void onDestroy()
+    {
+
+        super.onDestroy();
+        Log.i("Lifecycle", "onDestroy() invoked" );
     }
 }
